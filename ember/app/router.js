1017b9e719803e29d8129545ae270021657490f9
Devin Moss <devinsmoss@gmail.com>
2015-01-02 17:01:30 -0600

    Fixed router problem where posts.index route wasn't appearing

--- ember/app/router.js
+++ ember/app/router.js
@@ -7,7 +7,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route("about");
-  this.resource("posts");
+  this.resource("posts", function() {
+    //Nothing yet
+  });
 });
 
 export default Router;
